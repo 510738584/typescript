4c1faea056afd6d791d48c58bddb28d88bab929d
c15810829937 <510738584@qq.com>
2022-02-23 23:27:09 +0800

    ts模块化 lesson1

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,4 @@
-enum Permission {
-    Read = 1,   // 0001
-    Write = 2,  // 0010
-    Delete = 4, // 0100
-    Create = 8  // 1000
-};
-// 组合权限
-let p: Permission = Permission.Read | Permission.Write;
-// 判断是否包含权限
-function hasPermission(target:Permission, per:Permission) {
-    return (target & per) === per;
-}
-// console.log(hasPermission(p, Permission.Write));
-// 删除某个权限
-p = p ^ Permission.Write;
-console.log(hasPermission(p, Permission.Read));
+import { name, sum } from "./module";
 
-
-
+console.log(name);
+console.log(sum(1,3));
---
+++ src/module.ts
@@ -0,0 +1,4 @@
+export let name: string = 'power';
+export function sum(a: number, b:number): number{
+    return a + b;
+}
